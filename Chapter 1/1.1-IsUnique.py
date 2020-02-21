ba272be6d2a9902aeb2b5974116812b0738aa8ee
Daniel Lu <dcxia.zq@gmail.com>
2020-02-21 16:13:57 -0500

    Added LeetCode stuff

---
+++ Chapter 1/1.1-IsUnique.py
@@ -0,0 +1,16 @@
+#Implement and Algorithm to determine if a string has all unique characters. Do this in place. 
+
+
+#Allows for command line arguments
+import sys
+
+#print(sys.argv[1:])
+start = sys.argv[1:]
+
+htable = {} 
+
+htable['lul'] = 8 
+
+print(htable['lul'])
+
+
